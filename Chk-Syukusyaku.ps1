190bef7933d65e3c3dcca44b70f44da9d852c405
Hiroyuki Kimura <z_kimura@k-side.net>
2014-10-10 15:53:28 +0900

    縮尺の計算部分の記録開始

--- Chk-Syukusyaku.ps1
+++ Chk-Syukusyaku.ps1
@@ -1,10 +1,16 @@
+#初期値のセット
 #縮尺の文字列が記録されている位置の座標をセットする
 $strlocate = "'341.750000','230.750000'"
+#取得したいCAD座標系の名称をセットする
+$strLayer = "測量図"
+
+#処理の開始
 
 #SXFファイルから縮尺の文字列が記録された行を取り出す
 $strSyuku = Get-Content $ARGS[0] | Select-String "text_string_feature" | Select-String $strlocate
 
 #縮尺の文字列だけを取り出す
 $strSyuku = $strSyuku -Split ","
+$strSyuku = $strSyuku[3].Replace("\'","")
 
-echo $strSyuku[3].Replace("\'","")
+#
